986cf7eb2e42259ab2d5379c9eec4bfdb27469c4
Jason Day <jason.day.blue@gmail.com>
2021-11-29 21:13:24 -0600

    created local storage function

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ console.log(currentDate);
 let timeSlot = document.getElementById("currentDay");
 console.log(timeSlot.textContent);
 timeSlot.textContent = currentDate;
+let saveBtn9 = document.getElementById("saveBtn9");
+let text9 = document.getElementById("hour9");
 
-// $(".saveBtn").on("click", function () {});
+text9.textContent = localStorage.getItem("9AM");
+
+saveBtn9.addEventListener("click", function () {
+	let taValue = text9.value;
+	console.log(taValue);
+	localStorage.setItem("9AM", taValue);
+});
